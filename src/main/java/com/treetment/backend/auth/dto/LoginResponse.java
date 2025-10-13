82f5bbbaad1152e76dfa2af938c180e4b45dd68c
MuuiGong <muuigong@kakao.com>
2025-10-13 16:34:22 +0900

    Fix: 로그인 시 userId 반환하도록 수정

--- src/main/java/com/treetment/backend/auth/dto/LoginResponse.java
+++ src/main/java/com/treetment/backend/auth/dto/LoginResponse.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginResponse {
+    private Integer userId;
     private String email;
     private String name;
     private String nickname;
