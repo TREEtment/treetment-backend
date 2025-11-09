2d526a48d1c0aacd5590f508d807a090940d5a1e
MuuiGong <muuigong@kakao.com>
2025-11-09 23:34:26 +0900

    Fix: RestTemplate read Timeout 90초로 변경

--- src/main/java/com/treetment/backend/BackendApplication.java
+++ src/main/java/com/treetment/backend/BackendApplication.java
@@ -19,7 +19,7 @@ public class BackendApplication {
     public RestTemplate restTemplate() {
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
         factory.setConnectTimeout(5000); // 5초 연결 타임아웃
-        factory.setReadTimeout(30000); // 30초 읽기 타임아웃
+        factory.setReadTimeout(90000); // 90초 읽기 타임아웃
         return new RestTemplate(factory);
     }
 }
